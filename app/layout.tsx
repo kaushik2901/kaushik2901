6a98230b396d043ef65e2db045353bb314b94a53
Kaushik Jadav <kaushikjadav602@gmail.com>
2026-04-06 18:02:06 +0530

    feat: add all shad-cn components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,14 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const fontSans = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
-const geistMono = Geist_Mono({subsets:['latin'],variable:'--font-mono'})
+const geistMono = Geist_Mono({ subsets: ['latin'], variable: '--font-mono' })
 
 export default function RootLayout({
   children,
@@ -23,7 +24,11 @@ export default function RootLayout({
       className={cn("antialiased", fontSans.variable, "font-mono", geistMono.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <TooltipProvider>
+            {children}
+          </TooltipProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
